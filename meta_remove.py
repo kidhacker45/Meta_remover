cc85c18c163339adefe0cd3b3ff3f9c993f6e95f
KIDHACKER <129326203+kidhacker45@users.noreply.github.com>
2023-09-08 01:41:04 +0530

    Add files via upload

---
+++ meta_remove.py
@@ -0,0 +1,10 @@
+from PIL import Image
+image_path = str(input('[+] Enter Image path: '))
+image = Image.open(image_path)
+data = list(image.getdata())
+image_without_metadata = Image.new(image.mode, image.size)
+image_without_metadata.putdata(data)
+print("Metadata removed :")
+output_path = str(input('[+] Enter Image_remove path: '))
+image_without_metadata.save(output_path)
+print("file saved,(output_path) :")
